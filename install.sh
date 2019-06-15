ce6255dc7d8c1448bc2292265203762bb8312132
MichaIng <28480705+MichaIng@users.noreply.github.com>
2019-06-15 03:19:33 +0200

    Add support for Debian Buster
    Signed-off-by: Micha Felle <micha@dietpi.com>

--- install.sh
+++ install.sh
@@ -173,6 +173,9 @@ check_forked() {
 				fi
 				dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
 				case "$dist_version" in
+					10)
+						dist_version="buster"
+					;;
 					9)
 						dist_version="stretch"
 					;;
