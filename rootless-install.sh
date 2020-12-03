33f6c5310d52768344778ce7dd17da5337c3d30f
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-12-03 17:23:13 +0900

    rootless-install.sh: bump up test channel to 20.10.0-rc2
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- rootless-install.sh
+++ rootless-install.sh
@@ -30,7 +30,7 @@ if [ -z "$CHANNEL" ]; then
 fi
 # The latest release is currently hard-coded.
 STABLE_LATEST="19.03.14"
-TEST_LATEST="20.10.0-rc1"
+TEST_LATEST="20.10.0-rc2"
 STATIC_RELEASE_URL=
 STATIC_RELEASE_ROOTLESS_URL=
 case "$CHANNEL" in
