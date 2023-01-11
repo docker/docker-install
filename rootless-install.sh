5429d3131564d9f0f26a8e6bbb676cb3b6071f28
Sebastiaan van Stijn <github@gone.nl>
2023-01-11 23:46:52 +0100

    rootless: update test version to 23.0.0-rc.1
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- rootless-install.sh
+++ rootless-install.sh
@@ -30,7 +30,7 @@ if [ -z "$CHANNEL" ]; then
 fi
 # The latest release is currently hard-coded.
 STABLE_LATEST="20.10.22"
-TEST_LATEST="22.06.0-beta.0"
+TEST_LATEST="23.0.0-rc.1"
 STATIC_RELEASE_URL=
 STATIC_RELEASE_ROOTLESS_URL=
 case "$CHANNEL" in
