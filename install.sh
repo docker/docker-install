084d4ace9f8e67a0af6474a6f2b3df9fc2197d5d
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-02-11 02:40:52 +0900

    CentOS/RHEL: Use dnf if available
    ... instead of yum. The yum command is no longer available by default
    in recent versions.
    
    Closes #394
    
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- install.sh
+++ install.sh
@@ -559,19 +559,24 @@ do_install() {
 				echo "Packages for RHEL are currently only available for s390x."
 				exit 1
 			fi
-			if [ "$lsb_dist" = "fedora" ]; then
+
+			if command_exists dnf; then
 				pkg_manager="dnf"
 				config_manager="dnf config-manager"
 				enable_channel_flag="--set-enabled"
 				disable_channel_flag="--set-disabled"
 				pre_reqs="dnf-plugins-core"
-				pkg_suffix="fc$dist_version"
 			else
 				pkg_manager="yum"
 				config_manager="yum-config-manager"
 				enable_channel_flag="--enable"
 				disable_channel_flag="--disable"
 				pre_reqs="yum-utils"
+			fi
+
+			if [ "$lsb_dist" = "fedora" ]; then
+				pkg_suffix="fc$dist_version"
+			else
 				pkg_suffix="el"
 			fi
 			repo_file_url="$DOWNLOAD_URL/linux/$lsb_dist/$REPO_FILE"
