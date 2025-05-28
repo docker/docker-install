21b5eb82dccd503d6a82aff1ee12ba7ef3709538
Paweł Gronowski <pawel.gronowski@docker.com>
2025-05-29 00:42:58 +0200

    rootless: update to 28.2.1
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- rootless-install.sh
+++ rootless-install.sh
@@ -21,10 +21,10 @@ SCRIPT_COMMIT_SHA=UNKNOWN
 # This script should be run with an unprivileged user and install/setup Docker under $HOME/bin/.
 
 # latest version available in the stable channel.
-STABLE_LATEST="28.1.1"
+STABLE_LATEST="28.2.1"
 
 # latest version available in the test channel.
-TEST_LATEST="28.2.0-rc.1"
+TEST_LATEST="28.2.1"
 
 # The channel to install from:
 #   * test
